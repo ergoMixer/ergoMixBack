14ac4b8e19311a8ff324d6ff5ed02ff6a8de0c7c
anon2020zzz <anon2020zzz@protonmail.com>
2021-05-24 06:11:53 -0400

    Add release 3.3.5

--- mixer/app/mixer/NewMixer.scala
+++ mixer/app/mixer/NewMixer.scala
@@ -8,10 +8,12 @@ import db.Tables
 import helpers.ErgoMixerUtils
 import wallet.WalletHelper.now
 import javax.inject.Inject
+import models.Models
 import models.Models.MixStatus.{Queued, Running}
 import models.Models.MixWithdrawStatus.WithdrawRequested
 import models.Models.{Deposit, MixRequest, OutBox}
 import network.NetworkUtils
+import org.ergoplatform.appkit.SignedTransaction
 import play.api.Logger
 import wallet.Wallet
 
@@ -84,7 +86,7 @@ class NewMixer @Inject()(tables: Tables, aliceOrBob: AliceOrBob, ergoMixerUtils:
     val depositsToUse = getDeposits(depositAddress)
     val boxIds = depositsToUse.map(_.boxId)
     if (mixRequest.withdrawStatus.equals(WithdrawRequested.value)) { // withdrawing
-      if (shouldWithdraw(mixRequest.id, boxIds.mkString(","))) {
+      if (shouldWithdraw(id, boxIds.mkString(","))) {
         require(mixRequest.withdrawAddress.nonEmpty)
         val wallet = new Wallet(masterSecret)
         val secret = wallet.getSecret(-1).bigInteger
@@ -99,12 +101,25 @@ class NewMixer @Inject()(tables: Tables, aliceOrBob: AliceOrBob, ergoMixerUtils:
       return
     }
 
+    def updateTablesMixes(isAlice: Boolean, mixRequestId: String, time: Long, tx: SignedTransaction, depositsToUse: List[Models.Deposit], optTokenBoxId: Option[String]): Unit = {
+      depositsToUse.map { d =>
+        tables.insertSpentDeposit(d.address, d.boxId, d.amount, d.createdTime, d.tokenAmount, tx.getId, time, mixRequestId)
+        unspentDepositsTable.deleteWhere(boxIdCol === d.boxId)
+      }
+
+      mixRequestsTable.update(mixStatusCol <-- Running.value).where(mixIdCol === mixRequest.id)
+      spentTokenEmissionBoxTable.insert(mixRequest.id, optTokenBoxId.get)
+      mixStateTable.insert(mixRequestId, 0, isAlice)
+      tables.insertMixStateHistory(mixRequestId, round = 0, isAlice = isAlice, time = time)
+    }
+
     val avbl = depositsToUse.map(_.amount).sum
     val avblToken = depositsToUse.map(_.tokenAmount).sum
     val poolAmount = mixRequest.amount
     val numFeeToken = mixRequest.numToken
     val neededErg = mixRequest.neededAmount
     val neededToken = mixRequest.neededTokenAmount
+    // get a random token emission box
     val optTokenBoxId = getRandomValidBoxId(networkUtils.getTokenEmissionBoxes(numFeeToken, considerPool = true)
       .filterNot(box => spentTokenEmissionBoxTable.exists(boxIdCol === box.id)).map(_.id.toString))
 
@@ -113,7 +128,6 @@ class NewMixer @Inject()(tables: Tables, aliceOrBob: AliceOrBob, ergoMixerUtils:
 
     } else if (optTokenBoxId.isEmpty) {
       logger.warn("  No token emission box to get token from!")
-
     } else {
       // now we have enough balance, lets proceed to the first round of the queue ...
       // always try to initiate as Bob first, and if it fails, do as Alice
@@ -133,21 +147,21 @@ class NewMixer @Inject()(tables: Tables, aliceOrBob: AliceOrBob, ergoMixerUtils:
           && box.getToken(TokenErgoMix.tokenId) > 0).map(_.id)
       )
 
-      val (txId, isAlice) = if (optHalfMixBoxId.nonEmpty) {
+      if (optHalfMixBoxId.nonEmpty) {
         // half-mix box exists... behave as Bob
         val halfMixBoxId = optHalfMixBoxId.get
         val (fullMixTx, bit) = aliceOrBob.spendHalfMixBox(secret, halfMixBoxId, inputBoxIds :+ optTokenBoxId.get, Configs.startFee, depositAddress, Array(dLogSecret), broadCast = false, numFeeToken)
         val (left, right) = fullMixTx.getFullMixBoxes
         val bobFullMixBox = if (bit) right else left
-        tables.insertFullMix(mixRequest.id, round = 0, time = currentTime, halfMixBoxId, bobFullMixBox.id)
+        tables.insertFullMix(id, round = 0, time = currentTime, halfMixBoxId, bobFullMixBox.id)
         tables.insertTx(bobFullMixBox.id, fullMixTx.tx)
-        logger.info(s" [NEW: $id] --> Bob [halfMixBoxId:$halfMixBoxId, txId:${fullMixTx.tx.getId}]")
+        updateTablesMixes(isAlice = false, id, currentTime, fullMixTx.tx, depositsToUse, optTokenBoxId) // is not Alice
+        logger.info(s" [NEW: $id] --> Bob [halfMixBoxId:$halfMixBoxId, txId:${fullMixTx.tx.getId}] - before sendTransaction")
         val sendRes = ctx.sendTransaction(fullMixTx.tx)
         if (sendRes == null) logger.error(s"  something unexpected has happened! tx got refused by the node!")
-        (fullMixTx.tx.getId, false) // is not Alice
+        else logger.info(s"[txId: ${fullMixTx.tx.getId}] was broadcast.")
       } else {
         // half-mix box does not exist... behave as Alice
-        // get a random token emission box
 
         // do nothing
         if (Configs.mixOnlyAsBob) {
@@ -157,24 +171,14 @@ class NewMixer @Inject()(tables: Tables, aliceOrBob: AliceOrBob, ergoMixerUtils:
 
         val tx = aliceOrBob.createHalfMixBox(secret, inputBoxIds :+ optTokenBoxId.get, Configs.startFee,
           depositAddress, Array(dLogSecret), broadCast = false, poolAmount, numFeeToken, mixRequest.tokenId, mixRequest.mixingTokenAmount)
-        tables.insertHalfMix(mixRequest.id, round = 0, time = currentTime, tx.getHalfMixBox.id, isSpent = false)
+        tables.insertHalfMix(id, round = 0, time = currentTime, tx.getHalfMixBox.id, isSpent = false)
         tables.insertTx(tx.getHalfMixBox.id, tx.tx)
+        updateTablesMixes(isAlice = true, id, currentTime, tx.tx, depositsToUse, optTokenBoxId) // is Alice
+        logger.info(s" [NEW:$id] --> Alice [txId: ${tx.tx.getId}] - before sendTransaction")
         val sendRes = ctx.sendTransaction(tx.tx)
         if (sendRes == null) logger.info(s"  something unexpected has happened! tx got refused by the node!")
-        logger.info(s" [NEW:$id] --> Alice [txId: ${tx.tx.getId}]")
-        (tx.tx.getId, true) // is Alice
+        else logger.info(s"[txId: ${tx.tx.getId}] was broadcast")
       }
-
-      depositsToUse.map { d =>
-        tables.insertSpentDeposit(d.address, d.boxId, d.amount, d.createdTime, d.tokenAmount, txId, currentTime, id)
-        unspentDepositsTable.deleteWhere(boxIdCol === d.boxId)
-      }
-
-      mixRequestsTable.update(mixStatusCol <-- Running.value).where(mixIdCol === mixRequest.id)
-      spentTokenEmissionBoxTable.insert(mixRequest.id, optTokenBoxId.get)
-      mixStateTable.insert(mixRequest.id, 0, isAlice)
-      tables.insertMixStateHistory(mixRequest.id, round = 0, isAlice = isAlice, time = currentTime)
-
     }
   }
 
